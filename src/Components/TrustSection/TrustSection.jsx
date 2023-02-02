fc4b49f51f5cf2f7132692676e22051feb9fa681
maxi326519 <97068314+maxi326519@users.noreply.github.com>
2023-02-02 15:33:27 -0300

    Update wave

--- src/Components/TrustSection/TrustSection.jsx
+++ src/Components/TrustSection/TrustSection.jsx
@@ -29,18 +29,6 @@ export default function TrustSection() {
         </div>
         <p class="trust__bottom-text">¿QUÉ ESTÁS ESPERANDO PARA AUMENTAR TUS INGRESOS?</p>
       </div>
-      <svg
-        className="home__wave"
-        xmlns="http://www.w3.org/2000/svg"
-        preserveAspectRatio="none"
-        viewBox="0 0 1440 320"
-      >
-        <path
-          fill-opacity="1"
-          d="M0,64L60,96C120,128,240,192,360,186.7C480,181,600,107,720,112C840,117,960,203,1080,197.3C1200,192,1320,96,1380,48L1440,0L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"
-          data-darkreader-inline-fill=""
-        ></path>
-      </svg>
     </div>
   );
 }
